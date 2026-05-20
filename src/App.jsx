4b3427f0c2e193fb58c47031aed96a0683eb387a
dipar2848-byte <dipar2848@gmail.com>
2026-05-20 18:52:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./Pages/Home";
-import Services from "./Pages/Services";
-import Gallery from "./Pages/Gallery";
-import Contact from "./Pages/Contact";
-
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/gallery" element={<Gallery />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <h1 style={{
+      color: "white",
+      background: "black",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "50px"
+    }}>
+      TEST
+    </h1>
   );
 }
